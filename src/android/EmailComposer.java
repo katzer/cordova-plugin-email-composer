b903f39ea99602f6c3b44aa85db47cdebe753946
Sebastián Katzer <katzer.sebastian@googlemail.com>
2013-11-17 19:12:14 +0100

    Android support for upcomming v0.7.0 changes.

--- src/android/EmailComposer.java
+++ src/android/EmailComposer.java
@@ -26,173 +26,162 @@ import org.apache.cordova.PluginResult;
 
 public class EmailComposer extends CordovaPlugin {
 
-	private CallbackContext ctx;
+    private CallbackContext ctx;
 
-	@Override
-	public boolean execute (String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		// Eine E-Mail soll versendet werden
-		if ("open".equals(action)) {
-			open(args, callbackContext);
+    @Override
+    public boolean execute (String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        // Eine E-Mail soll versendet werden
+        if ("open".equals(action)) {
+            open(args, callbackContext);
 
-			return true;
-		}
+            return true;
+        }
 
-		// Es soll überprüft werden, ob ein Dienst zum Versenden der E-Mail zur Verfügung steht
-		if ("isServiceAvailable".equals(action)) {
-			isServiceAvailable(callbackContext);
+        // Es soll überprüft werden, ob ein Dienst zum Versenden der E-Mail zur Verfügung steht
+        if ("isServiceAvailable".equals(action)) {
+            isServiceAvailable(callbackContext);
 
-			return true;
-		}
+            return true;
+        }
 
-		// Returning false results in a "MethodNotFound" error.
-		return false;
-	}
+        // Returning false results in a "MethodNotFound" error.
+        return false;
+    }
 
-	/**
-	 * Überprüft, ob Emails versendet werden können.
-	 */
-	private void isServiceAvailable (CallbackContext ctx) {
-		Boolean available   = this.isEmailAccountConfigured();
-		PluginResult result = new PluginResult(PluginResult.Status.OK, available);
+    /**
+     * Überprüft, ob Emails versendet werden können.
+     */
+    private void isServiceAvailable (CallbackContext ctx) {
+        Boolean available   = this.isEmailAccountConfigured();
+        PluginResult result = new PluginResult(PluginResult.Status.OK, available);
 
-		ctx.sendPluginResult(result);
-	}
+        ctx.sendPluginResult(result);
+    }
 
-	/**
-	 * Öffnet den Email-Kontroller mit vorausgefüllten Daten.
-	 */
-	private void open (JSONArray args, CallbackContext ctx) throws JSONException {
-		JSONObject properties = args.getJSONObject(0);
-		Intent     draft      = this.getDraftWithProperties(properties);
+    /**
+     * Öffnet den Email-Kontroller mit vorausgefüllten Daten.
+     */
+    private void open (JSONArray args, CallbackContext ctx) throws JSONException {
+        JSONObject properties = args.getJSONObject(0);
+        Intent     draft      = this.getDraftWithProperties(properties);
 
-		this.ctx = ctx;
-		this.openDraft(draft);
-	}
+        this.ctx = ctx;
+        this.openDraft(draft);
+    }
 
-	/**
-	 * Erstellt den ViewController für Mails und fügt die übergebenen Eigenschaften ein.
-	 *
-	 * @param {JSONObject} params (Subject, Body, Recipients, ...)
-	 */
-	private Intent getDraftWithProperties (JSONObject params) throws JSONException {
-		Intent mail = new Intent(android.content.Intent.ACTION_SEND);
+    /**
+     * Erstellt den ViewController für Mails und fügt die übergebenen Eigenschaften ein.
+     *
+     * @param {JSONObject} params (Subject, Body, Recipients, ...)
+     */
+    private Intent getDraftWithProperties (JSONObject params) throws JSONException {
+        Intent mail = new Intent(android.content.Intent.ACTION_SEND);
 
-		if (params.has("subject"))
-			this.setSubject(params.getString("subject"), mail);
-		if (params.has("body"))
-			this.setBody(params.getString("body"), params.optBoolean("isHtml"), mail);
-		if (params.has("recipients"))
-			this.setRecipients(params.getJSONArray("recipients"), mail);
-		if (params.has("ccRecipients"))
-			this.setCcRecipients(params.getJSONArray("ccRecipients"), mail);
-		if (params.has("bccRecipients"))
-			this.setBccRecipients(params.getJSONArray("bccRecipients"), mail);
-		if (params.has("attachments"))
-			this.setAttachments(params.getJSONArray("attachments"), mail);
+        if (params.has("subject"))
+            this.setSubject(params.getString("subject"), mail);
+        if (params.has("body"))
+            this.setBody(params.getString("body"), params.optBoolean("isHtml"), mail);
+        if (params.has("to"))
+            this.setRecipients(params.getJSONArray("to"), mail);
+        if (params.has("cc"))
+            this.setCcRecipients(params.getJSONArray("cc"), mail);
+        if (params.has("bcc"))
+            this.setBccRecipients(params.getJSONArray("bcc"), mail);
+        if (params.has("attachments"))
+            this.setAttachments(params.getJSONArray("attachments"), mail);
 
-		mail.setType("application/octet-stream");
+        mail.setType("application/octet-stream");
 
-		return mail;
-	}
+        return mail;
+    }
 
-	/**
-	 * Zeigt den ViewController zum Versenden/Bearbeiten der Mail an.
-	 */
-	private void openDraft (Intent draft) {
-		this.cordova.startActivityForResult(this, Intent.createChooser(draft, "Select Email App"), 0);
-	}
+    /**
+     * Zeigt den ViewController zum Versenden/Bearbeiten der Mail an.
+     */
+    private void openDraft (Intent draft) {
+        this.cordova.startActivityForResult(this, Intent.createChooser(draft, "Select Email App"), 0);
+    }
 
-	/**
-	 * Setzt den Subject der Mail.
-	 */
-	private void setSubject (String subject, Intent draft) {
-		draft.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-	}
+    /**
+     * Setzt den Subject der Mail.
+     */
+    private void setSubject (String subject, Intent draft) {
+        draft.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+    }
 
-	/**
-	 * Setzt den Body der Mail.
-	 */
-	private void setBody (String body, Boolean isHTML, Intent draft) {
-		if (isHTML) {
-			draft.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml(body));
-			draft.setType("text/html");
-		} else {
-			draft.putExtra(android.content.Intent.EXTRA_TEXT, body);
-			draft.setType("text/plain");
-		}
-	}
+    /**
+     * Setzt den Body der Mail.
+     */
+    private void setBody (String body, Boolean isHTML, Intent draft) {
+        if (isHTML) {
+            draft.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml(body));
+            draft.setType("text/html");
+        } else {
+            draft.putExtra(android.content.Intent.EXTRA_TEXT, body);
+            draft.setType("text/plain");
+        }
+    }
 
-	/**
-	 * Setzt die Empfänger der Mail.
-	 */
-	private void setRecipients (JSONArray recipients, Intent draft) throws JSONException {
-		String[] receivers = new String[recipients.length()];
+    /**
+     * Setzt die Empfänger der Mail.
+     */
+    private void setRecipients (JSONArray recipients, Intent draft) throws JSONException {
+        String[] receivers = new String[recipients.length()];
 
-		for (int i = 0; i < recipients.length(); i++) {
-			receivers[i] = recipients.getString(i);
-		}
+        for (int i = 0; i < recipients.length(); i++) {
+            receivers[i] = recipients.getString(i);
+        }
 
-		draft.putExtra(android.content.Intent.EXTRA_EMAIL, receivers);
-	}
+        draft.putExtra(android.content.Intent.EXTRA_EMAIL, receivers);
+    }
 
-	/**
-	 * Setzt die CC-Empfänger der Mail.
-	 */
-	private void setCcRecipients (JSONArray ccRecipients, Intent draft) throws JSONException {
-		String[] receivers = new String[ccRecipients.length()];
+    /**
+     * Setzt die CC-Empfänger der Mail.
+     */
+    private void setCcRecipients (JSONArray ccRecipients, Intent draft) throws JSONException {
+        String[] receivers = new String[ccRecipients.length()];
 
-		for (int i = 0; i < ccRecipients.length(); i++) {
-			receivers[i] = ccRecipients.getString(i);
-		}
+        for (int i = 0; i < ccRecipients.length(); i++) {
+            receivers[i] = ccRecipients.getString(i);
+        }
 
-		draft.putExtra(android.content.Intent.EXTRA_CC, receivers);
-	}
+        draft.putExtra(android.content.Intent.EXTRA_CC, receivers);
+    }
 
-	/**
-	 * Setzt die BCC-Empfänger der Mail.
-	 */
-	private void setBccRecipients (JSONArray bccRecipients, Intent draft) throws JSONException {
-		String[] receivers = new String[bccRecipients.length()];
+    /**
+     * Setzt die BCC-Empfänger der Mail.
+     */
+    private void setBccRecipients (JSONArray bccRecipients, Intent draft) throws JSONException {
+        String[] receivers = new String[bccRecipients.length()];
 
-		for (int i = 0; i < bccRecipients.length(); i++) {
-			receivers[i] = bccRecipients.getString(i);
-		}
+        for (int i = 0; i < bccRecipients.length(); i++) {
+            receivers[i] = bccRecipients.getString(i);
+        }
 
-		draft.putExtra(android.content.Intent.EXTRA_BCC, receivers);
-	}
+        draft.putExtra(android.content.Intent.EXTRA_BCC, receivers);
+    }
 
-	/**
-	 * Fügt die Anhände zur Mail hinzu.
-	 * Convert from paths to Android friendly Parcelable Uri's
-	 */
-	private void setAttachments (JSONArray attachments, Intent draft) throws JSONException {
-		for (int i = 0; i < attachments.length(); i++) {
-			Uri attachmentUri = Uri.parse(attachments.getString(i));
-			File file = new File(attachmentUri.getPath());
-			if (file.exists()) {
-				draft.putExtra(Intent.EXTRA_STREAM, attachmentUri);
-			}
-		}
-	}
+    /**
+     * Fügt die Anhände zur Mail hinzu.
+     */
+    private void setAttachments (JSONArray attachments, Intent draft) throws JSONException {
+        for (int i = 0; i < attachments.length(); i++) {
+            Uri attachmentUri = Uri.parse(attachments.getString(i));
+            File file         = new File(attachmentUri.getPath());
 
-	/**
-	 * Gibt an, ob es eine Anwendung gibt, welche E-Mails versenden kann.
-	 */
-	private Boolean isEmailAccountConfigured () {
-		Intent  intent    = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","max@mustermann.com", null));
-		Boolean available = cordova.getActivity().getPackageManager().queryIntentActivities(intent, 0).size() > 1;
+            if (file.exists()) {
+                draft.putExtra(Intent.EXTRA_STREAM, attachmentUri);
+            }
+        }
+    }
 
-		return available;
-	}
+    /**
+     * Gibt an, ob es eine Anwendung gibt, welche E-Mails versenden kann.
+     */
+    private Boolean isEmailAccountConfigured () {
+        Intent  intent    = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","max@mustermann.com", null));
+        Boolean available = cordova.getActivity().getPackageManager().queryIntentActivities(intent, 0).size() > 1;
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-	 	super.onActivityResult(requestCode, resultCode, intent);
-
-	 	if (this.isEmailAccountConfigured()) {
-	 		this.ctx.success();
-	 	} else {
-	 		this.ctx.success(4);
-	 	}
-	}
+        return available;
+    }
 }
