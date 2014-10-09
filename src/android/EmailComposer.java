5288abf5676e436ee02eaee52c8d016259da58e2
Sebastián Katzer <katzer.sebastian@googlemail.com>
2014-10-09 17:48:43 +0200

    Removed `plugin` from package name

--- src/android/EmailComposer.java
+++ src/android/EmailComposer.java
@@ -19,7 +19,7 @@
     under the License.
 */
 
-package de.appplant.cordova.plugin.emailcomposer;
+package de.appplant.cordova.emailcomposer;
 
 import java.io.File;
 import java.io.FileOutputStream;
