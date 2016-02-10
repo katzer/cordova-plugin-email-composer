54beefcd8634123d51b23957b87fa04332322d75
bboybboy <kovalsky.v@gmail.com>
2016-02-10 18:12:00 +0200

    Updated plugin to cordova > 4.0

--- src/ios/APPEmailComposerImpl.m
+++ src/ios/APPEmailComposerImpl.m
@@ -418,7 +418,7 @@
 #ifndef __CORDOVA_3_8_0
     NSData* data = [NSData dataFromBase64String:dataString];
 #else
-    NSData* data = [NSData cdv_dataFromBase64String:dataString];
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:dataString options:NSDataBase64DecodingIgnoreUnknownCharacters];
 #endif
 
     return data;
