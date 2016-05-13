5a0000cab3b08cbf1398151c1ea32f6bd4f2bce0
juhasuni <juhis.suni@gmail.com>
2016-05-13 14:23:42 +0300

    Update EmailComposerImpl.java

--- src/android/EmailComposerImpl.java
+++ src/android/EmailComposerImpl.java
@@ -46,6 +46,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
+import android.util.Patterns;
 
 import static de.appplant.cordova.emailcomposer.EmailComposer.LOG_TAG;
 
