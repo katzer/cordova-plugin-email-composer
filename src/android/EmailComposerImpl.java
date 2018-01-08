58ef57f2859bcdfb1cc6e642bc9943599e34724d
Sebastián Katzer <katzer.sebastian@googlemail.com>
2018-01-08 12:31:03 +0100

    Move asset methods into own class

--- src/android/EmailComposerImpl.java
+++ src/android/EmailComposerImpl.java
@@ -24,11 +24,8 @@ import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.text.Html;
-import android.util.Base64;
 import android.util.Log;
 import android.util.Patterns;
 
@@ -37,23 +34,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import static de.appplant.cordova.emailcomposer.AssetUtil.ATTACHMENT_FOLDER;
 import static de.appplant.cordova.emailcomposer.EmailComposer.LOG_TAG;
 
 class EmailComposerImpl {
 
     // The default mailto: scheme.
     static private final String MAILTO_SCHEME = "mailto:";
-
-    // Path where to put tmp the attachments.
-    static private final String ATTACHMENT_FOLDER = "/email_composer";
 
     /**
      * Cleans the attachment folder.
@@ -206,9 +196,10 @@ class EmailComposerImpl {
                                  Context ctx) throws JSONException {
 
         ArrayList<Uri> uris = new ArrayList<Uri>();
+        AssetUtil assets    = new AssetUtil(ctx);
 
         for (int i = 0; i < attachments.length(); i++) {
-            Uri uri = getUriForPath(attachments.getString(i), ctx);
+            Uri uri = assets.parse(attachments.getString(i));
             if (uri != null) uris.add(uri);
         }
 
@@ -225,297 +216,6 @@ class EmailComposerImpl {
 
         draft.setAction(Intent.ACTION_SEND)
                 .putExtra(Intent.EXTRA_STREAM, uris.get(0));
-    }
-
-    /**
-     * The URI for an attachment path.
-     *
-     * @param path  The given path to the attachment.
-     * @param ctx   The application context.
-     * @return      The URI pointing to the given path.
-     */
-    private Uri getUriForPath (String path, Context ctx) {
-        if (path.startsWith("res:")) {
-            return getUriForResourcePath(path, ctx);
-        } else if (path.startsWith("app://")) {
-            return getUriForAppInternalPath(path, ctx);
-        } else if (path.startsWith("file:///")) {
-            return getUriForAbsolutePath(path, ctx);
-        } else if (path.startsWith("file://")) {
-            return getUriForAssetPath(path, ctx);
-        } else if (path.startsWith("base64:")) {
-            return getUriForBase64Content(path, ctx);
-        }
-
-        return Uri.parse(path);
-    }
-
-    /**
-     * The URI for a file.
-     *
-     * @param path  The given absolute path.
-     * @param ctx   The application context.
-     * @return      The URI pointing to the given path.
-     */
-    private Uri getUriForAbsolutePath (String path, Context ctx) {
-        String absPath = path.replaceFirst("file://", "");
-        File file      = new File(absPath);
-
-        if (!file.exists()) {
-            Log.e(LOG_TAG, "File not found: " + file.getAbsolutePath());
-        }
-
-        return getUriForFile(ctx, file);
-    }
-
-    /**
-     * The URI for an asset.
-     *
-     * @param path  The given asset path.
-     * @param ctx   The application context.
-     * @return      The URI pointing to the given path.
-     */
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private Uri getUriForAssetPath (String path, Context ctx) {
-        String resPath  = path.replaceFirst("file:/", "www");
-        String fileName = resPath.substring(resPath.lastIndexOf('/') + 1);
-        File dir        = ctx.getExternalCacheDir();
-
-        if (dir == null) {
-            Log.e(LOG_TAG, "Missing external cache dir");
-            return Uri.EMPTY;
-        }
-
-        String storage  = dir.toString() + ATTACHMENT_FOLDER;
-        File file       = new File(storage, fileName);
-
-        new File(storage).mkdir();
-
-        FileOutputStream outStream = null;
-
-        try {
-            AssetManager assets = ctx.getAssets();
-
-            outStream = new FileOutputStream(file);
-            InputStream inputStream    = assets.open(resPath);
-
-            copyFile(inputStream, outStream);
-            outStream.flush();
-            outStream.close();
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "File not found: assets/" + resPath);
-            e.printStackTrace();
-        } finally {
-            if (outStream != null) {
-                safeClose(outStream);
-            }
-        }
-
-        return getUriForFile(ctx, file);
-    }
-
-    /**
-     * The URI for an internal file.
-     *
-     * @param path The given asset path.
-     * @param ctx  The application context.
-     * @return     The URI pointing to the given path.
-     */
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private Uri getUriForAppInternalPath (String path, Context ctx) {
-        String resPath  = path.replaceFirst("app:/", "");
-        String fileName = resPath.substring(resPath.lastIndexOf('/') + 1);
-        File dir        = ctx.getExternalCacheDir();
-
-        if (dir == null) {
-            Log.e("EmailComposer", "Missing external cache dir");
-            return Uri.EMPTY;
-        }
-
-        String storage  = dir.toString() + ATTACHMENT_FOLDER;
-        File file       = new File(storage, fileName);
-
-        new File(storage).mkdir();
-        File privateDir    = ctx.getFilesDir();
-        String privatePath = privateDir.getAbsolutePath()+"/.."+resPath;
-
-        try {
-            FileOutputStream outStream = new FileOutputStream(file);
-            InputStream inputStream    = new FileInputStream(privatePath);
-
-            copyFile(inputStream, outStream);
-            outStream.flush();
-            outStream.close();
-        } catch (Exception e) {
-            Log.e("EmailComposer", "File not found: " + privatePath);
-            e.printStackTrace();
-        }
-
-        return getUriForFile(ctx, file);
-    }
-
-    /**
-     * The URI for a resource.
-     *
-     * @param path  The given relative path.
-     * @param ctx   The application context.
-     * @return      The URI pointing to the given path
-     */
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private Uri getUriForResourcePath (String path, Context ctx) {
-        String resPath   = path.replaceFirst("res://", "");
-        String fileName  = resPath.substring(resPath.lastIndexOf('/') + 1);
-        String resName   = fileName.substring(0, fileName.lastIndexOf('.'));
-        String extension = resPath.substring(resPath.lastIndexOf('.'));
-        File dir         = ctx.getExternalCacheDir();
-
-        if (dir == null) {
-            Log.e(LOG_TAG, "Missing external cache dir");
-            return Uri.EMPTY;
-        }
-
-        String storage   = dir.toString() + ATTACHMENT_FOLDER;
-        int resId        = getResId(resPath, ctx);
-        File file        = new File(storage, resName + extension);
-
-        if (resId == 0) {
-            Log.e(LOG_TAG, "File not found: " + resPath);
-        }
-
-        new File(storage).mkdir();
-
-        FileOutputStream outStream = null;
-
-        try {
-            Resources res = ctx.getResources();
-            outStream = new FileOutputStream(file);
-            InputStream inputStream    = res.openRawResource(resId);
-
-            copyFile(inputStream, outStream);
-            outStream.flush();
-            outStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (outStream != null) {
-                safeClose(outStream);
-            }
-        }
-
-        return getUriForFile(ctx, file);
-    }
-
-    /**
-     * The URI for a base64 encoded content.
-     *
-     * @param content   The given base64 encoded content.
-     * @param ctx       The application context.
-     * @return          The URI including the given content.
-     */
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private Uri getUriForBase64Content (String content, Context ctx) {
-        String resName = content.substring(content.indexOf(":") + 1, content.indexOf("//"));
-        String resData = content.substring(content.indexOf("//") + 2);
-        File dir       = ctx.getExternalCacheDir();
-        byte[] bytes;
-
-        try {
-            bytes = Base64.decode(resData, 0);
-        } catch (Exception ignored) {
-            Log.e(LOG_TAG, "Invalid Base64 string");
-            return Uri.EMPTY;
-        }
-
-        if (dir == null) {
-            Log.e(LOG_TAG, "Missing external cache dir");
-            return Uri.EMPTY;
-        }
-
-        String storage = dir.toString() + ATTACHMENT_FOLDER;
-        File file      = new File(storage, resName);
-
-        new File(storage).mkdir();
-
-        FileOutputStream outStream = null;
-
-        try {
-            outStream = new FileOutputStream(file);
-
-            outStream.write(bytes);
-            outStream.flush();
-            outStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (outStream != null) {
-                safeClose(outStream);
-            }
-        }
-
-        return getUriForFile(ctx, file);
-    }
-
-    /**
-     * Writes an InputStream to an OutputStream
-     *
-     * @param in    The input stream.
-     * @param out   The output stream.
-     */
-    private void copyFile (InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[1024];
-        int read;
-
-        while ((read = in.read(buffer)) != -1) {
-            out.write(buffer, 0, read);
-        }
-    }
-
-    /**
-     * Returns the resource ID for the given resource path.
-     *
-     * @param ctx   The application context.
-     * @return      The resource ID for the given resource.
-     */
-    private int getResId (String resPath, Context ctx) {
-        Resources res = ctx.getResources();
-        int resId;
-
-        String pkgName  = ctx.getPackageName();
-        String dirName  = "drawable";
-        String fileName = resPath;
-
-        if (resPath.contains("/")) {
-            dirName  = resPath.substring(0, resPath.lastIndexOf('/'));
-            fileName = resPath.substring(resPath.lastIndexOf('/') + 1);
-        }
-
-        String resName = fileName.substring(0, fileName.lastIndexOf('.'));
-
-        resId = res.getIdentifier(resName, dirName, pkgName);
-
-        if (resId == 0) {
-            resId = res.getIdentifier(resName, "mipmap", pkgName);
-        }
-
-        if (resId == 0) {
-            resId = res.getIdentifier(resName, "drawable", pkgName);
-        }
-
-        return resId;
-    }
-
-    /**
-     * Get content URI for the specified file.
-     *
-     * @param ctx The application context.
-     * @param file The file to get the URI.
-     *
-     * @return content://...
-     */
-    private Uri getUriForFile(Context ctx, File file) {
-        String authority = ctx.getPackageName() + ".provider";
-
-        return EmailComposerProvider.getUriForFile(ctx, authority, file);
     }
 
     /**
@@ -578,26 +278,6 @@ class EmailComposerImpl {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         return intent;
-    }
-
-    /**
-     * Attempt to safely close the given stream.
-     *
-     * @param outStream The stream to close.
-     * @return          true if successful, false otherwise
-     */
-    private static boolean safeClose (final FileOutputStream outStream) {
-
-        if (outStream != null) {
-            try {
-                outStream.close();
-                return true;
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error attempting to safely close resource: " + e.getMessage());
-            }
-        }
-
-        return false;
     }
 
 }
